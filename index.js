489d85b3914afcee0fe27534611067bcd5233873
lelbil <billelattouchi@yahoo.fr>
2017-06-08 16:50:47 +0000

    Working version

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ var express = require('express')
 var app = express();
 
 app.get("/", function(req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hello real world");
+	//res.writeHead(200, {"Content-Type": "text/html"});
+	res.sendFile(__dirname + "/home.html");
+}); 
+
+app.get("/:id", function(req, res) {
+	//res.writeHead(200, {"Content-Type": "text/json"});
+	var id = (/^\d+$/.test(req.params.id))? Math.floor(parseInt(req.params.id)*1000) : req.params.id; 
+	var d = new Date(id);
+	if (!isNaN(d.getTime())) {
+		res.json({"unixtime": Math.floor(Date.parse(d)/1000), "natural": d.toDateString()});	
+	} else {
+		res.json({"unixtime": null, "natural": null});
+	}
+
 });
 
 app.listen(8080);
+console.log("server listening on port 8080..");
