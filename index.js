53f2eb7f629d69fd4723fa29dd49e35b50c98dad
lelbil <billelattouchi@yahoo.fr>
2017-06-09 10:07:24 +0000

    another stupid TYPO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 
 var app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", function(req, res) {
 	//res.writeHead(200, {"Content-Type": "text/html"});
